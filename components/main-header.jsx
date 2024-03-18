319e5985cc6b4ee7af680a9fd2865cd564106517
donnydevanda <devandadonny@gmail.com>
2024-03-18 22:02:05 +0700

    delete unused files & navbar setup

--- components/main-header.jsx
+++
@@ -1,7 +0,0 @@
-export default MainHeader() {
-    return <header>
-        <Link href="/">
-            
-        </Link>
-    </header>
-}
---
+++ components/main-header/main-header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="a platter with food in it" />
+          FoodMood
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
